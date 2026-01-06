942712c71ef04765bb3d1797c3b94c9addcfd084
Clouddyyyy <leveringcharlotte@gmail.com>
2026-01-06 10:49:43 +0100

    made rotate, saturation and blur!

---
+++ DOMOefening/main.js
@@ -0,0 +1,36 @@
+<<<<<<< HEAD
+const imgElement =  document.getElementById("image");
+
+const saturationButton  = document.getElementById("saturation");
+
+function saturationButtonClicked(){
+    imgElement.classList.add("saturation");
+}
+
+saturationButton.addEventListener("click", saturationButtonClicked);
+
+const blurButton = document.getElementById("blur");
+
+function blurButtonClicked(){
+    imgElement.classList.add("blur");
+}
+blurButton.addEventListener("click", blurButtonClicked);
+
+const somethingButton = document.getElementById("something");
+
+function somethingButtonClicked(){
+    imgElement.classList.add("something");
+}
+
+somethingButton.addEventListener("click", somethingButtonClicked);
+
+
+=======
+const imgElement = document.getElementById("image");
+
+const saturationButton = document.getElementById("saturation");
+function saturationButtonClicked(){
+    imgElement.classList.toggle("saturation");
+}
+saturationButton.addEventListener("click", saturationButtonClicked);
+>>>>>>> e5493fc2948118d0dabf7ed002e729e8417773aa
